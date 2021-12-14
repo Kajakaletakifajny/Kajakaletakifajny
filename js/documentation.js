fe532e9039846b31f41747f9c7e259a257f5b2cf
Just-iiiiiiiiiiiiiin <wyslijtokajaki@gmail.com>
2021-12-14 09:01:36 +0100

    wysylanie

---
+++ js/documentation.js
@@ -0,0 +1,26 @@
+/**
+ * @descryption return sum of two elements
+ * @param {anytype}first component of sum
+ * @param {anytype}second component of sum
+ * @returns {anytype}sum of two components
+ */
+function sum(first, second)
+{
+    return first + second;
+}
+
+alert(sum(2,3));
+
+/**
+ * @param {Number}first component of multiplication
+ * @param {Number}second component of multiplication
+ * @param {Number}third component of multiplication
+ * @returns {anytype} multiplication of three components
+ */
+
+function multiplication(first, second, third)
+{
+    return first * second * third;
+}
+
+alert(multiplication(1,2,3));
