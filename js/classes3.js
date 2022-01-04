88b03fb1ca9ab3c43f605e341991e241b2edd88c
Kajakaletakifajny <wyslijtokajaki@gmail.com>
2022-01-04 09:07:26 +0100

    a

---
+++ js/classes3.js
@@ -0,0 +1,14 @@
+class student {
+    private PESEL = "";
+    private name ='';
+    private surname = "";
+
+    public getPESEL(){
+        getPESEL=this.PESEL;
+        return getCena;
+    }
+    public getName(){
+        getName=this.name;
+        return GetName;
+    }
+}
